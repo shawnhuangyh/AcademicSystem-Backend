e90898a57029bc398da153a7fc83364a477ebae7
Shawn Huang <shawn.huang.y.h@gmail.com>
2023-04-23 12:27:59 +0800

    feat: add new app

--- AcademicSystem/settings.py
+++ AcademicSystem/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'AcademicSystem.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "School",
+        "USER": "bastian",
+        "PASSWORD": "hKT7zjA@oJ8GkehFphjj",
+        "HOST": "circle.shawnhuangyh.com",
+        "PORT": 3306,
     }
 }
 
