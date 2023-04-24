25ea3b558e9589acd00a4759e43da842a152a9fc
Shawn Huang <shawn.huang.y.h@gmail.com>
2023-04-24 19:03:38 +0800

    feat: update models

--- AcademicSystem/settings.py
+++ AcademicSystem/settings.py
@@ -166,3 +166,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+AUTH_USER_MODEL = 'App.User'
--- App/models.py
+++ App/models.py
@@ -1,20 +1,15 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class Administrator(models.Model):
-    admin_id = models.CharField(primary_key=True, max_length=100)
-    name = models.CharField(max_length=100, blank=True, null=True)
+class User(AbstractUser):
+    pass
 
-    class Meta:
-        managed = False
-        db_table = 'administrator'
+
+class Administrator(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    admin_id = models.CharField(max_length=100, blank=True, null=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
 
 
 class Class(models.Model):
@@ -28,10 +23,6 @@ class Class(models.Model):
     current_selection = models.IntegerField(blank=True, null=True)
     max_selection = models.IntegerField(blank=True, null=True)
 
-    class Meta:
-        managed = False
-        db_table = 'class'
-
 
 class Course(models.Model):
     course_id = models.AutoField(primary_key=True)
@@ -39,22 +30,15 @@ class Course(models.Model):
     gp_percentage = models.FloatField(blank=True, null=True, db_comment='General Performance Percentage')
     dept = models.ForeignKey('Department', models.DO_NOTHING, blank=True, null=True)
 
-    class Meta:
-        managed = False
-        db_table = 'course'
-
 
 class CourseSelection(models.Model):
     course_selection_id = models.AutoField(primary_key=True)
     student = models.ForeignKey('Student', models.DO_NOTHING)
-    class_field = models.ForeignKey(Class, models.DO_NOTHING, db_column='class_id')  # Field renamed because it was a Python reserved word.
+    class_field = models.ForeignKey(Class, models.DO_NOTHING,
+                                    db_column='class_id')  # Field renamed because it was a Python reserved word.
     gp = models.FloatField(blank=True, null=True, db_comment='General Performance')
     exam = models.FloatField(blank=True, null=True)
     grade = models.FloatField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'course_selection'
 
 
 class Department(models.Model):
@@ -63,18 +47,10 @@ class Department(models.Model):
     address = models.CharField(max_length=9999, blank=True, null=True)
     phone = models.CharField(max_length=100, blank=True, null=True)
 
-    class Meta:
-        managed = False
-        db_table = 'department'
-
 
 class Major(models.Model):
     major_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'major'
 
 
 class MajorPlan(models.Model):
@@ -82,37 +58,23 @@ class MajorPlan(models.Model):
     major = models.ForeignKey(Major, models.DO_NOTHING)
     course = models.ForeignKey(Course, models.DO_NOTHING)
 
-    class Meta:
-        managed = False
-        db_table = 'major_plan'
-
 
 class Semester(models.Model):
     semester_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, blank=True, null=True)
 
-    class Meta:
-        managed = False
-        db_table = 'semester'
-
 
 class Student(models.Model):
-    student_id = models.CharField(primary_key=True, max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    student_id = models.CharField(max_length=100, blank=True, null=True)
     name = models.CharField(max_length=100, blank=True, null=True)
     gpa = models.FloatField(blank=True, null=True)
     dept = models.ForeignKey(Department, models.DO_NOTHING)
     major = models.ForeignKey(Major, models.DO_NOTHING)
 
-    class Meta:
-        managed = False
-        db_table = 'student'
-
 
 class Teacher(models.Model):
-    teacher_id = models.CharField(primary_key=True, max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    teacher_id = models.CharField(max_length=100, blank=True, null=True)
     name = models.CharField(max_length=100, blank=True, null=True)
     dept = models.ForeignKey(Department, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'teacher'
