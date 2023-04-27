a5925731d0f58967693e623456fe4c3c3d86be6c
Shawn Huang <shawn.huang.y.h@gmail.com>
2023-04-27 19:03:03 +0800

    fix: update errors in all tables using foreign keys

--- App/serializers/course_selection.py
+++ App/serializers/course_selection.py
@@ -30,6 +30,20 @@ class CourseSelectionSerializer(serializers.HyperlinkedModelSerializer):
         validated_data.update(student=student, class_field=class_field)
         return super(CourseSelectionSerializer, self).create(validated_data)
 
+    def update(self, instance, validated_data):
+        course_selection_id = self.data.get('course_selection_id', None)
+        course_selection = CourseSelection.objects.get(pk=course_selection_id)
+        student = course_selection.student
+        class_field = course_selection.class_field
+        student_id = self.initial_data.get('student_id', None)
+        class_id = self.initial_data.get('class_id', None)
+        if student_id is not None:
+            student = Student.objects.get(pk=student_id)
+        if class_id is not None:
+            class_field = Student.objects.get(pk=class_field)
+        validated_data.update(student=student, class_field=class_field)
+        return super(CourseSelectionSerializer, self).update(course_selection, validated_data)
+
 
 class StudentSelectSerializer(serializers.HyperlinkedModelSerializer):
     student = StudentSerializer(read_only=True)
@@ -52,3 +66,17 @@ class StudentSelectSerializer(serializers.HyperlinkedModelSerializer):
         class_field = Class.objects.get(pk=class_id)
         validated_data.update(student=student, class_field=class_field)
         return super(StudentSelectSerializer, self).create(validated_data)
+
+    def update(self, instance, validated_data):
+        course_selection_id = self.data.get('course_selection_id', None)
+        course_selection = CourseSelection.objects.get(pk=course_selection_id)
+        student = course_selection.student
+        class_field = course_selection.class_field
+        student_id = self.initial_data.get('student_id', None)
+        class_id = self.initial_data.get('class_id', None)
+        if student_id is not None:
+            student = Student.objects.get(pk=student_id)
+        if class_id is not None:
+            class_field = Student.objects.get(pk=class_field)
+        validated_data.update(student=student, class_field=class_field)
+        return super(StudentSelectSerializer, self).update(course_selection, validated_data)
--- App/serializers/teacher.py
+++ App/serializers/teacher.py
@@ -21,3 +21,13 @@ class TeacherSerializer(serializers.HyperlinkedModelSerializer):
         dept = Department.objects.get(pk=dept_id)
         validated_data.update(dept=dept)
         return super(TeacherSerializer, self).create(validated_data)
+
+    def update(self, instance, validated_data):
+        teacher_id = self.data.get('teacher_id', None)
+        teacher = Teacher.objects.get(teacher_id=teacher_id)
+        dept = teacher.dept
+        dept_id = self.initial_data.get('dept_id', None)
+        if dept_id is not None:
+            dept = Department.objects.get(pk=dept_id)
+        validated_data.update(dept=dept)
+        return super(TeacherSerializer, self).update(teacher, validated_data)
